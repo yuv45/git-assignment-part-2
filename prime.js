bf266e523e53dfc824d4a4e3d996405d43349098
Mihir Soni <ms6711534@gmail.com>
2023-08-25 18:29:57 +0530

    My task is done

--- prime.js
+++ prime.js
@@ -1,11 +1,11 @@
 function check_prime(number){
-  let count =0;
+  let count = 0;
   for(let i=0;i<=number;i++){
     if(number%i==0){
-      count+2
+      count++
     }
     
   }
-    (count==10)?console.log("The no.is prime"):console.log("The no.is prime")
+    (count==2)?console.log("The no.is prime"):console.log("The no.is not prime")
 }
-check_prime(13}		
+check_prime(13);	
