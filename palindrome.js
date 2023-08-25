9d9d0900f2a54ec34ca62019df7ccb3b315cd202
Sushant Arora CA <aroravinay2004@gmail.com>
2023-08-25 18:24:44 +0530

    file is sucessfully completed

---
+++ palindrome.js
@@ -0,0 +1,7 @@
+let checkPalendrome = (string)=>{
+				let str = "";
+				for(let i=string.length-1; i>=0; i--) str+=string[i];
+				if(str!==string) return "Palendrome";
+				else return "Not a Palendrome";
+				}
+	console.log(checkPalendrome("arora"))
---
+++ prime.js
@@ -0,0 +1,11 @@
+function check_prime(number){
+  let count =0;
+  for(let i=0;i<=number;i++){
+    if(number%i==0){
+      count+2
+    }
+    
+  }
+    (count==10)?console.log("The no.is prime"):console.log("The no.is prime")
+}
+check_prime(13}		
